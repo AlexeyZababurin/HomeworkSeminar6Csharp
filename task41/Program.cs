27f8454a583631a200d55dc96f6b9a4dfc6a0adb
Alexey <Lehan347@yandex.ru>
2023-07-26 14:47:47 +0300

    The program was written for task 41

--- task41/Program.cs
+++ task41/Program.cs
@@ -3,62 +3,33 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите числа через запятую: ");
-int[] numbers = StringToNum(Console.ReadLine());
-PrintArray(numbers);
-int sum = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0)
-    {
-        sum++;
-    }
-}
-Console.WriteLine();
-Console.WriteLine($"Количество чисел больше нуля = {sum}");
-
-
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
-
-    int[] numbers = new int [count];
-    int index = 0;
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
-
-void PrintArray(int[] array)
-{
-    Console.Write(" ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + ", ");
-    }
-    Console.Write(" ");
-}
+int FindPositiveNumbers(string[] array) 
+{ 
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        string temp = array[i]; 
+        int temp1 = Convert.ToInt32(temp); 
+        if (i == array.Length - 1) 
+        { 
+            Console.Write(temp); 
+        } 
+        else 
+        { 
+            Console.Write(temp); 
+            Console.Write(", "); 
+        } 
+        if (temp1 > 0) count++; 
+    } 
+    return count; 
+} 
+ 
+string[] CreateArray(string text) 
+{ 
+    Console.Write("Введите числа через запятую: "); 
+    return Console.ReadLine().Split(','); 
+} 
+ 
+string[] array = CreateArray("Введите числа через запятую: "); 
+int count = FindPositiveNumbers(array); 
+Console.WriteLine($" -> {count}");
